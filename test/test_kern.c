0128ce3cb0847ae542185696a214600acc756565
crj-gmail <caoruijuan13@gmail.com>
2020-04-16 17:10:19 +0800

    test_kern.c xadd

--- test/test_kern.c
+++ test/test_kern.c
@@ -16,16 +16,28 @@ struct bpf_map_def SEC("maps") bpf_pass_map =
 	.max_entries= XDP_ACTION_MAX,
 };
 
+//fetch and add value to ptr
+#ifndef lock_xadd
+#define lock_xadd(ptr, val) ((void) __sync_fetch_and_add(ptr, val))
+#endif
+
+/* This is the data record stored in the map */
+struct datarec {
+	__u64 rx_packets;
+	/* Assignment#1: Add byte counters */
+};
+
 SEC("xdp_pass")
 int xdp_pass_func(struct xdp_md *ctx)
 {	
-	int *pkt_count;
+	struct datarec *pkt;
 	__u32 key = XDP_PASS;
-	pkt_count = bpf_map_lookup_elem(&bpf_pass_map, &key);
-	if (!pkt_count)
+	pkt = bpf_map_lookup_elem(&bpf_pass_map, &key);
+	if (!pkt)
+	{
 		return XDP_ABORTED;
-
-	__sync_fetch_and_add(&pkt_count, 1);	
+	}
+	lock_xadd(&pkt->rx_packets, 1);	
 
 	return XDP_PASS;
 }
