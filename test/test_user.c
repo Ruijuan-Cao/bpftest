909600381b831bbbad42d8769ba886223b33682a
crj-gmail <caoruijuan13@gmail.com>
2020-04-20 16:39:10 +0800

    test_user.c debug error

--- test/test_user.c
+++ test/test_user.c
@@ -233,8 +233,6 @@ static struct xsk_socket_info *xsk_configure_socket(struct xsk_umem_info *umem, 
 	if (!txr)
 		printf("txr is NULL\n");
 
-	printf("umem size = %d\n", umem->umem.fd);
-
 	//crate xsk
 	int ret = xsk_socket__create(&xsk->xsk, opt_if, opt_queue, umem->umem, rxr, txr, &cfg);
 	if(ret)
