d14a6971f0000ca7d804891ba103a8b6cb41a63c
crj-gmail <caoruijuan13@gmail.com>
2020-05-26 16:54:43 +0800

    push poller print stats map info

--- test/test_kern.c
+++ test/test_kern.c
@@ -48,7 +48,7 @@ struct bpf_map_def SEC("maps") xdp_stats_map =
 #define lock_xadd(ptr, val) ((void) __sync_fetch_and_add(ptr, val))
 #endif
 
-/*
+
 SEC("xdp_pass")
 int xdp_pass_func(struct xdp_md *ctx)
 {	
@@ -56,32 +56,6 @@ int xdp_pass_func(struct xdp_md *ctx)
 	__u32 key = XDP_PASS;
 	pkt = bpf_map_lookup_elem(&bpf_pass_map, &key);
 	if (!pkt)
-		return XDP_ABORTED;
-
-	lock_xadd(&pkt->rx_packets, 1);	
-
-	return XDP_PASS;
-}
-*/
-
-/*
- *	struct vlan_hdr - vlan header
- *	@h_vlan_TCI: priority and VLAN ID
- *	@h_vlan_encapsulated_proto: packet type ID or len
- */
-
-struct vlan_hdr {
-	__be16	h_vlan_TCI;
-	__be16	h_vlan_encapsulated_proto;
-};
-
-SEC("filter")
-int xdp_filter(struct xdp_md *ctx)
-{
-	//get map pointer
-	__u32 key = XDP_PASS;
-	struct datarec *rec = bpf_map_lookup_elem(&xdp_stats_map, &key);
-	if (!rec)
 		return XDP_ABORTED;
 
 	//get data header
@@ -142,6 +116,90 @@ int xdp_filter(struct xdp_md *ctx)
 		}
 	}
 
+	lock_xadd(&pkt->rx_packets, 1);	
+
+	return XDP_PASS;
+}
+
+
+/*
+ *	struct vlan_hdr - vlan header
+ *	@h_vlan_TCI: priority and VLAN ID
+ *	@h_vlan_encapsulated_proto: packet type ID or len
+ */
+
+struct vlan_hdr {
+	__be16	h_vlan_TCI;
+	__be16	h_vlan_encapsulated_proto;
+};
+
+SEC("filter")
+int xdp_filter(struct xdp_md *ctx)
+{
+	//get map pointer
+	__u32 key = XDP_PASS;
+	struct datarec *rec = bpf_map_lookup_elem(&xdp_stats_map, &key);
+	if (!rec)
+		return XDP_ABORTED;
+/*
+	//get data header
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	struct ethhdr *eth = data;
+
+	//check size
+	__u64 addr_off = sizeof(*eth);
+	if (data + addr_off > data_end){
+		lock_xadd(&rec->rx_packets, 1);
+		return XDP_PASS;
+	}
+
+	__u64 h_proto = eth->h_proto;
+
+	//vlan - handle doubel VLAN tagged packet
+	for(int i = 0; i < 2; i++){
+		if (h_proto == htons(ETH_P_8021Q) || h_proto == htons(ETH_P_8021AD)){
+			struct vlan_hdr *vhdr = data + addr_off;
+			addr_off += sizeof(struct vlan_hdr);
+			if (data + addr_off > data_end){
+				lock_xadd(&rec->rx_packets, 1);
+				return XDP_PASS;
+			}
+			h_proto = vhdr->h_vlan_encapsulated_proto;
+		}
+	}
+
+	//ipv4
+	if (h_proto == htons(ETH_P_IP)){
+		struct iphdr *iph = data + addr_off;
+		struct udphdr *udph = data + addr_off + sizeof(struct iphdr);
+		if (udph + 1 > (struct udphdr *)data_end){
+			lock_xadd(&rec->rx_packets, 1);
+			return XDP_PASS;
+		}
+		//UDP
+		if (iph->protocol == IPPROTO_UDP 
+			//source address
+			&& (htonl(iph->saddr) & 0xFFFFFF00) == 0xC0A8E300
+			&& udph->dest == htons(12345) ){
+				rec->saddr = htonl(iph->saddr);
+				return XDP_DROP;
+		}
+	}
+	else if (h_proto == htons(ETH_P_IPV6)){
+		struct ipv6hdr * ipv6h = data + addr_off;
+		struct udphdr * udph = data + addr_off + sizeof(struct ipv6hdr);
+		if (udph + 1 > (struct udphdr *)data_end)
+			return XDP_PASS;
+		if (ipv6h->nexthdr == IPPROTO_UDP 
+			&& ipv6h->daddr.s6_addr[0] == 0xfd
+			&& ipv6h->daddr.s6_addr[1] == 0x00
+			&& udph->dest == htons(12345)){
+			//rec->daddr = htonl(ipv6h->daddr);
+			return XDP_DROP;	
+		}
+	}
+*/
 	lock_xadd(&rec->rx_packets, 1);
     return XDP_PASS;
 }
